af513db238af7038c68cdf39f330945c3387f017
Philippe Proulx <eeppeliteloop@gmail.com>
2024-05-02 10:57:34 -0400

    Update copyright info
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- formol.py
+++ formol.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Philippe Proulx <eeppeliteloop@gmail.com>
+# Copyright (c) 2024 Philippe Proulx <eepp.ca>
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
